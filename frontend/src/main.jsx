c9041651c38d2052c68d4ed819213cb04a8e929c
lizzz <cherkashina.liza.8b@gmail.com>
2025-12-16 14:38:53 +0300

    frontend yaaay

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <App />,
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App/App'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
